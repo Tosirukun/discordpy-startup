a21871f1f41a93bc96597062078cbde1a7ce2d75
Tosirukun(とじるくん) <72246321+Tosirukun@users.noreply.github.com>
2021-03-16 13:58:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,11 @@
 from discord.ext import commands
 import os
 import traceback
+import requests
+import json
+import discord
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='tn!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -16,6 +19,12 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.command()
+async def server(ctx):
+    body = requests.get("https://api.tosirukun.tk/api/tapi?type=server").text
+    data = json.loads(body)
+    await ctx.send(embed=discord.Embed(title="とじるくんネットワークのステータス").add_field(name="現在の接続数", value=data["data"]["online"]).add_field(name="接続可能な最大数", value=data["data"]["max"]))
 
 
 bot.run(token)
